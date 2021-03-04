15bf53f381882f43b597bfced7627493fa235070
Carlo Emilio Montanari <carlidel95@gmail.com>
2021-03-04 11:16:22 +0100

    I AM RETARDED!

--- data_handler.py
+++ data_handler.py
@@ -50,7 +50,7 @@ class data_handler(object):
 
 
 ### Data location on EOS
-data_path = "/home/camontan/Insync/carlo.montanari3@studio.unibo.it/OneDriveBiz/projects/dyn_indicator_analysis/data"
+data_path = "/mnt/volume"
 
 #### DATASET STANDARD ####
 
